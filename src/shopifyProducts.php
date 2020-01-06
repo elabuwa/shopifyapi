72d22cae6e83b0277d15cbae65045591fd1147f9
Dileep Ratnayake <dileep_ratnayake@hotmail.com>
2020-01-07 09:15:24 +1300

    products API

---
+++ src/shopifyProducts.php
@@ -0,0 +1,46 @@
+<?php
+namespace shopifyApi;
+
+use shopifyApi\shopifyApiCore;
+
+class shopifyProducts extends shopifyApiCore{
+
+    /**
+     * The construct function. send credentials provided by Shopify to instantiate object
+     *
+     * @param string $userName
+     * @param string $password
+     * @param string $shopifyUrl
+     * @param string $apiVersion
+     */
+    public function __construct($userName,$password,$shopifyUrl,$apiVersion)
+    {
+        $this->userName = $userName;
+        $this->password = $password;
+        $this->storeShopifyUrl = $shopifyUrl;
+        $this->apiVersion = $apiVersion;
+        parent::__construct();
+    }
+
+    /**
+     * Retrieve all products from store
+     * Send params array to define extra search params
+     * 
+     * Todo : Read response header to get additional pages where product count is more than 250
+     *
+     * @param array $params
+     * @return void
+     */
+    public function retrieveAllProducts($params = [])
+    {
+        if(!array_key_exists('limit', $params)){
+            //If no limit has been defined, grab 250 (the maximum given by Shopify for a request)
+            $params['limit'] = 250;
+        }
+        $this->queryUrl = $this->baseUrl . "products.json";
+        $response = $this->getData($params);
+        return $response;
+    }
+
+
+}
