9faee69bd4aeb5b2177123e32eb0783bce42a143
Dileep Ratnayake <dileep_ratnayake@hotmail.com>
2020-04-12 17:05:10 +1200

    new store info endpoint

---
+++ src/shopifyStore.php
@@ -0,0 +1,50 @@
+<?php
+namespace shopifyApi;
+
+use shopifyApi\shopifyApiCore;
+
+class shopifyStore extends shopifyApiCore{
+
+    /**
+     * The construct function. send credentials provided by Shopify to instantiate object
+     *
+     * @param string $userName
+     * @param string $password
+     * @param string $shopifyUrl
+     * @param string $apiVersion
+     */
+    public function __construct($credentials = [])
+    {
+        if(array_key_exists('userName', $credentials)){
+            $this->userName = $credentials['userName'];
+        }
+        if(array_key_exists('password', $credentials)){
+            $this->password = $credentials['password'];
+        }
+        if(array_key_exists('apiVersion', $credentials)){
+            $this->apiVersion = $credentials['apiVersion'];
+        }
+        if(array_key_exists('storeShopifyUrl', $credentials)){
+            $this->storeShopifyUrl = $credentials['storeShopifyUrl'];
+        }
+        if(array_key_exists('accessToken', $credentials)){
+            $this->accessToken = $credentials['accessToken'];
+        }
+        parent::__construct();
+    }
+
+    /**
+     * Retrieve all customers from store
+     * Send params array to define extra search params
+     *
+     * @param array $params
+     * @return void
+     */
+    public function storeInformation()
+    {
+        $this->queryUrl = $this->baseUrl . "shop.json";
+        $response = $this->getData();
+        return $response;
+    }
+
+}
